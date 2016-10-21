d6b0c3f481eef113439d2fca650d3848fef24cbc
b4b4r07 <b4b4r07@gmail.com>
2016-10-21 13:10:17 +0900

    Comment-out number option

--- src/arguments.sh
+++ src/arguments.sh
@@ -4,6 +4,7 @@ __enhancd::arguments::option()
     shift
 
     cat "$ENHANCD_ROOT/src/custom/config.ltsv" \
+        | __enhancd::utils::grep -v '^(//|#)' \
         | awk -F$'\t' '/:'"$opt"'\t/{print $4}' \
         | cut -d: -f2 \
         | read action
