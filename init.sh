1745218431232bf414029b8c729005334046ab02
asakasa <asakasa@me.com>
2017-01-08 15:28:20 +0900

    Fix that shwordsplit doesn't work properly.
    `setopt localoptions SH_WORD_SPLIG` is unneeded, because shwordsplit
    isn't enabled in global environment. Instead `eval` command use to
    execute the `$filter` command.
    
    This patch was reverted in part by 1c41d74c52f81db8fd2410cbe897e9a8c0fc7590

--- init.sh
+++ init.sh
@@ -61,19 +61,7 @@ __enhancd::init::init()
         ENHANCD_FILTER="fzy:fzf-tmux:fzf:peco:percol:gof:pick:icepick:sentaku:selecta"
     fi
 
-    # In zsh it will cause field splitting to be performed
-    # on unquoted parameter expansions.
-    if __enhancd::utils::has "setopt" && [[ -n $ZSH_VERSION ]]; then
-        # Note in particular the fact that words of unquoted parameters are not
-        # automatically split on whitespace unless the option SH_WORD_SPLIT is set;
-        # see references to this option below for more details.
-        # This is an important difference from other shells.
-        # (Zsh Manual 14.3 Parameter Expansion)
-        setopt localoptions SH_WORD_SPLIT
-    fi
-
     ENHANCD_AWK="$(__enhancd::utils::awk)"
-
 }
 
 __enhancd::init::init
