4c6656201e3f70b2e9bc0b29a42fcfeda0109541
b4b4r07 <b4b4r07@gmail.com>
2019-06-10 22:14:53 +0900

    Refactor some

--- src/ltsv.sh
+++ src/ltsv.sh
@@ -17,10 +17,6 @@ __enhancd::ltsv::open()
 
 __enhancd::ltsv::parse()
 {
-    if [[ ! -p /dev/stdin ]]; then
-        return 1
-    fi
-
     local -a args
     local query
     while (( $# > 0 ))
@@ -32,6 +28,12 @@ __enhancd::ltsv::parse()
                 ;;
             -v)
                 args+=("-v" "$2")
+                shift
+                ;;
+            -f)
+                args+=("-f" "$ENHANCD_ROOT/lib/ltsv.awk")
+                args+=("-f" "$2")
+                query=""
                 shift
                 ;;
         esac
@@ -47,8 +49,11 @@ __enhancd::ltsv::parse()
 
 __enhancd::ltsv::get()
 {
-    local opt="${1}" key="${2}"
+    local opt="${1:?value of key short or long required}" key="${2}"
     __enhancd::ltsv::open \
         | __enhancd::filter::exclude_commented \
-        | __enhancd::ltsv::parse -v opt="${opt}" -v key="${key}" -q 'ltsv("short")==opt||ltsv("long")==opt{print ltsv(key)}'
+        | __enhancd::ltsv::parse \
+        -v opt="${opt}" \
+        -v key="${key}" \
+        -q 'ltsv("short")==opt||ltsv("long")==opt{print key=="" ? $0 : ltsv(key)}'
 }
