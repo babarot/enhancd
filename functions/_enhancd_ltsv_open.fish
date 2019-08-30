fbeaae9b43bb55f1357306b4dc528c040916b633
Matthieu <mmacnab22@gmail.com>
2019-08-30 22:15:47 +0200

    Fix options not showing when print help message

--- functions/_enhancd_ltsv_open.fish
+++ functions/_enhancd_ltsv_open.fish
@@ -1,9 +1,6 @@
 function _enhancd_ltsv_open
-    set -l configs
-    set -a configs "$ENHANCD_ROOT/config.ltsv"
-    set -a configs "$ENHANCD_DIR/config.ltsv"
+    set -l configs "$ENHANCD_ROOT/config.ltsv"
 
-    set -l config
     for config in "$configs"
         if [ -f "$config" ]
             cat "$config"
