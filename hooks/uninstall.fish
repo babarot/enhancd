cf1683d8d8d41233d3b54b8b766772ffb22f4ccb
Gazorby <gazorby@pm.me>
2020-11-04 16:23:44 +0100

    ♻️ refactor!: drop support for omf, update to fisher 4

--- hooks/uninstall.fish
+++
@@ -1,1 +0,0 @@
-__enhancd_uninstall
--- uninstall.fish
+++
@@ -1,6 +0,0 @@
-# enhancd-fish uninstall hook
-#
-# You can use this file to do custom cleanup when the package is uninstalled.
-# You can use the variable $path to access the package path.
-
-rm --force --recursive --dir $ENHANCD_DIR
