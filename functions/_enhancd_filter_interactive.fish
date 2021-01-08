031833d78376bb18476b582a291cb63bd56102a5
David Dorfman <david.dorfman@granulate.io>
2021-01-08 20:40:31 +0200

    Add option to abbreviate home directory to ~ when searching

--- functions/_enhancd_filter_interactive.fish
+++ functions/_enhancd_filter_interactive.fish
@@ -13,6 +13,22 @@ function _enhancd_filter_interactive
     set -l filter (_enhancd_filepath_split_list "$ENHANCD_FILTER")
     set -l count (echo "$stdin" | _enhancd_command_grep -c "")
 
+    if test $ENHANCD_FILTER_ABBREV = 1
+        function _enhancd_filter_interactive_abbrev
+            while read -l line
+                string replace --regex "^$HOME" "~" "$line"
+            end
+        end
+
+        function _enhancd_filter_interactive_expand
+            while read -l line
+                string replace --regex "^~" "$HOME" "$line"
+            end
+        end
+
+        set filter "_enhancd_filter_interactive_abbrev | $filter | _enhancd_filter_interactive_expand"
+    end
+
     switch "$count"
         case '1'
             if test -n "$stdin"
