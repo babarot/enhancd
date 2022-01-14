c3f48cca06bcff96fd81cbecd270b106148742ac
Rishi K <contact.rishikmr@gmail.com>
2022-01-14 11:57:03 +0530

    fix: optimize computation of `ENHANCD_ROOT` in case of Bash shell.

--- init.sh
+++ init.sh
@@ -22,7 +22,7 @@ _ENHANCD_FAILURE=60
 
 if [[ -n $BASH_VERSION ]]; then
     # BASH
-    ENHANCD_ROOT="$(builtin cd "$(command dirname "$BASH_SOURCE")" && pwd)"
+    ENHANCD_ROOT="$( dirname "${BASH_SOURCE[0]}" )"
 elif [[ -n $ZSH_VERSION ]]; then
     # ZSH
     ENHANCD_ROOT="${${(%):-%x}:A:h}"
