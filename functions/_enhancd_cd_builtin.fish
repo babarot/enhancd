f0e8f7175a4322477bb600817dda7ff8dffbde86
Dash <33206210+LordFlashmeow@users.noreply.github.com>
2022-11-21 12:07:24 -0800

    fish: Do not change directory if Ctrl-C pressed in fuzzy search
    In fish, running `cd foobar` will cd to the home directory (effectively running `cd` with no arguments) if the target directory does not exist.
    The same behavior happens if `Ctrl-C` or `Esc` is pressed while the fuzzy search is open.
    
    Related: #161 
    
    This fix will make enhancd default to the working directory if no arguments are passed in, since that only happens when there is an error.
    
    Fix suggested by @Gorthog in #161

--- functions/_enhancd_cd_builtin.fish
+++ functions/_enhancd_cd_builtin.fish
@@ -1,5 +1,11 @@
 function _enhancd_cd_builtin
     set -l code 0
+    
+    # in case nothing was passed in argv, default to working directory
+    if test -z "$argv"
+        set argv (pwd)
+    end
+    
     _enhancd_cd_before
     builtin cd $argv
     set code $status
