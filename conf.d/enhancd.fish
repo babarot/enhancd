8c29cc033cbddd4ab97a500951390c56e4e7a433
b4b4r07 <b4b4r07@gmail.com>
2023-03-27 23:25:23 +0900

    Bump for new feature

--- conf.d/enhancd.fish
+++ conf.d/enhancd.fish
@@ -22,7 +22,7 @@ function __enhancd_install --on-event enhancd_install
 
     set -Ux ENHANCD_COMPLETION_KEYBIND "^I"
 
-    set -Ux _ENHANCD_VERSION "2.3.1"
+    set -Ux _ENHANCD_VERSION "2.4.0"
 
     # Set the filters if empty
     set -Ux ENHANCD_FILTER "fzy:fzf:peco:percol:sk:zf:gof:selecta"
