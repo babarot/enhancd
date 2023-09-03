2b4216174a3a695e565374d1ade10ce63fcd72f0
gazorby <gazorby@pm.me>
2023-09-03 05:53:02 +0200

    fix: fuzzy search

--- functions/_enhancd_filter_fuzzy.fish
+++ functions/_enhancd_filter_fuzzy.fish
@@ -1,9 +1,13 @@
 function _enhancd_filter_fuzzy
+    read --list --local stdin
     if test -z "$argv[1]"
-        command cat <&0
+        echo $stdin
     else
-        _enhancd_command_awk \
+        set -l dirs (
+            string join \n -- $stdin | _enhancd_command_awk \
             -f "$ENHANCD_ROOT/lib/fuzzy.awk" \
             -v search_string="$argv[1]"
+        )
+        echo $dirs
     end
 end
