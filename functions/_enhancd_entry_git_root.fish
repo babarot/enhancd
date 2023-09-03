5d67161cbea3abcb36efc0c9f952cf849af09179
gazorby <gazorby@pm.me>
2023-09-03 04:35:38 +0200

    refactor: remove old functions

--- functions/_enhancd_entry_git_root.fish
+++
@@ -1,8 +0,0 @@
-function _enhancd_entry_git_root
-    if git rev-parse --is-inside-work-tree > /dev/null 2>&1
-       and test (git rev-parse --show-toplevel) != (pwd)
-        git rev-parse --show-toplevel 2> /dev/null | _enhancd_filter_exclude "true"
-    else
-        return
-    end
-end
