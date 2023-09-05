f71aa886cd5d4686c66ef2bc39ad204719813531
gazorby <gazorby@pm.me>
2023-09-05 18:50:23 +0200

    fix: fix history update

--- functions/_enhancd_history_update.fish
+++ functions/_enhancd_history_update.fish
@@ -1,6 +1,6 @@
 function _enhancd_history_update
     begin
-        _enhancd_history_exists "$PWD"; or _enhancd_filepath_walk
+        _enhancd_history_exists; or _enhancd_filepath_walk
         _enhancd_history_open
         echo "$HOME"
     end | _enhancd_filter_reverse \
