ed9d17c740bd7748764b061c10517ead107d8a18
gazorby <gazorby@pm.me>
2023-09-07 18:53:02 +0200

    fix(fish): alt+f not triggering the filter

--- functions/_enhancd_complete.fish
+++ functions/_enhancd_complete.fish
@@ -1,7 +1,9 @@
 function _enhancd_complete
-    set -l query (_enhancd_source_home)/
+    set -l query (_enhancd_source_home | _enhancd_filter_interactive)
+
     if test -n "$query"
         commandline -t "$query"
     end
-    commandline -f repaint
+
+    commandline --functions repaint
 end
