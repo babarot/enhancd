576c0e5e2a12975ba00869d98722b46ffa7be3a2
Jesper Brix Rosenkilde <jbr@humppa.dk>
2025-01-01 03:50:42 +0100

    Use CDPATH if set (#219)
    * Respect CDPATH when set
    
    * Fish version

--- functions/_enhancd_source_history.fish
+++ functions/_enhancd_source_history.fish
@@ -6,5 +6,12 @@ function _enhancd_source_history
         return 0
     end
 
+    if test -n "$CDPATH"
+        _enhancd_command_awk \
+            -f "$ENHANCD_ROOT/lib/cdpath.awk" \
+            -v cdpath="$CDPATH" \
+            -v dir="$dir" && return 0
+     end
+
     _enhancd_history_list "$dir"
 end
